891e9f0a70646c9b2ccbf32dbdeff3e642f0cd77
Hemal Badola <badolahemal@gmail.com>
2025-11-14 09:11:37 +0530

    chore: temporarily hardcode production Firebase config

--- citemesh-ui/src/firebase.ts
+++ citemesh-ui/src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyD_siEWuKuU04fY69ImUxbGwbN9txxKKj8',
+  authDomain: 'citemesh.firebaseapp.com',
+  projectId: 'citemesh',
+  storageBucket: 'citemesh.firebasestorage.app',
+  messagingSenderId: '63495277735',
+  appId: '1:63495277735:web:f1b48fe795294e3ff89f82',
 };
 
 // Initialize Firebase
